aab6ee213f024404b9652565ddd990c5e59a27b9
nathalierocelle <70849363+nathalierocelle@users.noreply.github.com>
2022-08-04 17:08:59 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -33,29 +33,29 @@ def main():
     state_factor = st.selectbox("State in which the building is located", options=option_state_factor)
     building_class = st.selectbox("Building Class", options=option_building_class)
     year_built = st.slider("Year in which the building was constructed", 1600, 2016)
-    energy_star_rating = st.slider("Energy Star Rating of the building", 0.0, 100.0,0.1)
+    energy_star_rating = st.slider("Energy Star Rating of the building", 0.0, 100.0)
     floor_area = st.slider("Floor area (square feet) of the building", 943, 6385382)
     
     #environment of the building
-    days_below_20F = st.slider("Days with below 20F temperature",0, 93,1)
-    days_below_10F = st.slider("Days with below 10F temperature",0, 59,1)
-    days_above_100F = st.slider("Days with above 100F temperature",0, 1,1)   
-    heating_degree = st.slider("Total months where the daily average temperature falls under 65 Fahreinheit",33,660,1)
+    days_below_20F = st.slider("Days with below 20F temperature",0, 93)
+    days_below_10F = st.slider("Days with below 10F temperature",0, 59)
+    days_above_100F = st.slider("Days with above 100F temperature",0, 1)   
+    heating_degree = st.slider("Total months where the daily average temperature falls under 65 Fahreinheit",33,660)
     
-    spring_avg_temp = st.slider("Average temperature (in Fahreinheit) during spring season at building location",41.5, 75.15,0.1)
-    fall_avg_temp = st.slider("Average temperature (in Fahreinheit) during fall season at building location",47.0, 79.0,0.1)
-    summer_min_temp = st.slider("Minimum temperature (in Fahreinheit) during summer season at building location",30.0, 68.0,0.1)
-    min_temp = st.slider("Minimum temperature (in Fahreinheit) at building location",-19.0, 44.0,0.1)
-    avg_temp = st.slider("Average temperature (in Fahreinheit) at building location",44.0, 77.0,0.1)
+    spring_avg_temp = st.slider("Average temperature (in Fahreinheit) during spring season at building location",41.5, 75.15)
+    fall_avg_temp = st.slider("Average temperature (in Fahreinheit) during fall season at building location",47.0, 79.0)
+    summer_min_temp = st.slider("Minimum temperature (in Fahreinheit) during summer season at building location",30.0, 68.0)
+    min_temp = st.slider("Minimum temperature (in Fahreinheit) at building location",-19.0, 44.0)
+    avg_temp = st.slider("Average temperature (in Fahreinheit) at building location",44.0, 77.0)
     
-    max_wind_speed = st.slider("Maximum wind speed recorded in the building site",1.0, 23.3,0.1)
-    direction_max_wind_speed = st.slider("Direction of the maximum wind speed in the building site",1,360,1)
+    max_wind_speed = st.slider("Maximum wind speed recorded in the building site",1.0, 23.3)
+    direction_max_wind_speed = st.slider("Direction of the maximum wind speed in the building site",1,360)
     
-    snowdepth_inches = st.slider("Annual snow depth (inches) at the building site",0, 1292,1)
-    snowfall_inches = st.slider("Annual snow fall (inches) at the building site",0.0, 127.0,0.1)
-    precipitation_inches = st.slider("Annual precipitation (inches) at the location of the building",0.0, 112.92,0.1)  
+    snowdepth_inches = st.slider("Annual snow depth (inches) at the building site",0, 1292)
+    snowfall_inches = st.slider("Annual snow fall (inches) at the building site",0.0, 127.0)
+    precipitation_inches = st.slider("Annual precipitation (inches) at the location of the building",0.0, 112.92)  
            
-    submit = st.form_submit_button("Predict")
+    submit = st.button("Predict")
 
     if submit:
         facility_type = encode_value(facility_type,option_facility_type)
